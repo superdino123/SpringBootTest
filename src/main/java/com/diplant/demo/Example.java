a21bc44ddc9c1bf1470b7b93561e7f7134f00ddd
unknown <1119674493@qq.com>
2019-05-08 19:52:28 +0800

    springboottest

---
+++ src/main/java/com/diplant/demo/Example.java
@@ -0,0 +1,20 @@
+package com.diplant.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+	
+	@RequestMapping("/")
+	public String home(){
+		return "hello world!";
+	}
+	
+	public static void main(String[] args) throws Exception{
+		SpringApplication.run(Example.class, args);
+	}
+}
